c80cc4352c8f39a6b6a66b9be98adc1af5a7a748
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-05-03 21:34:43 +0200

    setup post to return correct static json object

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -14,10 +16,10 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.post('/api/shorturl', function(req, res) {
+  console.log(req.body)
+  res.json({"original_url":"https://www.google.com","short_url":1})
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
