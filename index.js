dee031cd650b35b8e89d71f883d01126d6c58558
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-10-28 23:08:24 +0200

    create url shortener functions

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+const db = require('./config/dbConfig');
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+let urlArray = [];
+let shortUrlCount = 0;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -12,15 +17,36 @@ app.use(express.json());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+
+app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post('/api/shorturl', function(req, res) {
+app.get('/api/shorturl/:id', async (req, res) => {
+
+  const query = await db.query(`SELECT url FROM urls WHERE url_id = ${req.params.id}`);
+
+  if (query.length === 0) {
+    res.json({ error: 'invalid url' });
+  } else {
+    res.redirect(query[0].url);
+  }
+});
+
+app.post('/api/shorturl', async (req, res) => {
   const urlPattern = /\w{4}:\/\/\w{3}.\w+.com/g;
   
   if (urlPattern.test(req.body.url)) {
-    res.json({"original_url":"https://www.google.com","short_url":1})
+
+    const queryUrlExists = await db.query(`SELECT url_id FROM urls WHERE url = '${req.body.url}'`);
+
+    if (queryUrlExists.length > 0) {
+      res.json({"original_url": req.body.url , "short_url": queryUrlExists[0].url_id})
+    } else {
+      const addUrl = await db.query(`INSERT INTO urls (url) VALUES ('${req.body.url}') RETURNING url_id, url`);
+
+      res.json({"original_url": req.body.url , "short_url": addUrl[0].url_id});
+    }
   } else {
     res.json({ error: 'invalid url' });
   }
